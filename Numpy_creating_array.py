4fd26c63da5c2e404f5b4199a9183c3da01001be
Rakesh <RK4528005@gmail.com>
2025-03-01 20:25:59 +0530

    commit this

---
+++ Numpy_creating_array.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([[1,3,4,5,6,7,8],["Rakesj",2,3,4,5,6,7]])
+print(arr.ndim) # To check the dimensions
+print(arr.shape) #To check number of columns and rows (2,7)
+print(arr.size) # Total number of elemets 14
+print(arr.dtype) # Type of data
+
+
